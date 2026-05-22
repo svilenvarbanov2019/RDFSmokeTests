273f4e0eabcf717807c5f05c132cba3f0c356741
Svilen M. Varbanov <svilen.varbanov@rdf.bg>
2026-05-22 17:33:18 +0300

    Compilation/Link errors.

--- CppEngineTests/Example_RDFGeomDefine.cpp
+++ CppEngineTests/Example_RDFGeomDefine.cpp
@@ -21,8 +21,8 @@ static double GetLength(OwlInstance inst)
     ASSERT(prop);
 
     double* rval = NULL;
-    int_t card = -1;
-    if (0==GetDatatypeProperty(inst, prop, (void**)&rval, &card) && rval && card > 0) {
+    int64_t card = -1;
+    if (0==GetDatatypeProperty(inst, prop, (const void**)&rval, &card) && rval && card > 0) {
         return rval[0];
     }
 

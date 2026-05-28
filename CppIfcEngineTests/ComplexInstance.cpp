08a7c57f57a834346560611cc495e314fce5d5db
Svilen M. Varbanov <svilen.varbanov@rdf.bg>
2026-05-28 09:37:15 +0300

    Fix: Save by Stream Callback.

--- CppIfcEngineTests/ComplexInstance.cpp
+++ CppIfcEngineTests/ComplexInstance.cpp
@@ -54,7 +54,7 @@ static void CheckComplex(SdaiModel model, ExpressID id, const char* entityName, 
 //
 const int_t BLOCK_LENGTH_WRITE = 20000; //  no maximum limit
 static FILE* myFileWrite = nullptr;
-static void    __stdcall   WriteCallBackFunction(unsigned char* content, int64_t size)
+static void    __stdcall   WriteCallBackFunction(unsigned char* content, int_t size)
 {
     fwrite(content, (size_t)size, 1, myFileWrite);
 }
